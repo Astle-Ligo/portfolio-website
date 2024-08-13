20e6f4a22313c9e0559adeb3061e9fd6e6c580a3
Astle-Ligo <astlelijo04@gmail.com>
2024-08-13 06:33:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".gsapcontainer",
+    pin: true,
+    scrub: 1,
+    // snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".gsapcontainer").offsetWidth
+  }
+});
+
+
